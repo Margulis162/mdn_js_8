2313c20c43977e4cfb13e582dfa7e7de1eada22d
Margulis162 <markmargulis@proton.me>
2023-08-15 14:33:42 -0400

    objects can be created using object constractors

--- js.js
+++ js.js
@@ -15,3 +15,14 @@ const person = {
 const myDataName = "height";
 const myDataValue = "1.75m";
 person[myDataName] = myDataValue;
+
+function Person(name) {
+    this.name = name;
+    this.introduceSelf = function(){
+        console.log(`Hi! I'm ${this.name}.`);
+    };
+}
+
+const mark = new Person("Mark");
+
+console.log(  mark.introduceSelf(), mark.name);
